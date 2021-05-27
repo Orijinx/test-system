8365fa70049b74b9e40327e142cfff9d7f6d1fe3
Vlad <vladonnx@mail.ru>
2021-05-27 19:13:31 +0300

    Delete utils

--- public/json.php
+++
@@ -1,3 +0,0 @@
-<?php
- header('Content-Type: application/json');
-     echo json_encode('[{id: 1,question: "Сколько лет создателю?",answer: "",}, {id: 3,question: "Его любимый язык программирования?",answer: "",}]', JSON_UNESCAPED_UNICODE);
